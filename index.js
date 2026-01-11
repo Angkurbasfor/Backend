39a9afbd35183ef861639c107cb2915abbf5f437
Angkurbasfor <binodbasfor85@gmail.com>
2026-01-11 15:35:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ import {
 const token = process.env.BOT_TOKEN;
 const REWARD_AMOUNT = 500;
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+        const firebaseConfig = {
+  apiKey: "AIzaSyDZwLLI4Ske7m4Ch1FI8jniU0Ayor8LVRM",
+  authDomain: "telegram-mini-6932e.firebaseapp.com",
+  projectId: "telegram-mini-6932e",
+  storageBucket: "telegram-mini-6932e.firebasestorage.app",
+  messagingSenderId: "1007549666090",
+  appId: "1:1007549666090:web:84af388b9d6f2798430453"
 };
 
 // Initialize Firebase & Bot
